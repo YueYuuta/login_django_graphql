dd5ebd73dd6e7cd0dbeb9a42b8d947a44c486b39
YueYuuta <48193808+YueYuuta@users.noreply.github.com>
2021-02-16 11:59:05 -0500

    first commit

---
+++ core/db.py
@@ -0,0 +1,10 @@
+POSTGRES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'python_graphql',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432'
+    }
+}
